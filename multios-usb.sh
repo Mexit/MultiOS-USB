4b922032dd26759563f4dd64f7a5c47d454740f0
Daniel Rudolf <github.com@daniel-rudolf.de>
2026-09-02 14:23:28 +0200

    Auto-select default data filesystem
    If `mkfs.exfat` is installed, use `exfat` as default filesystem, but fall back to `fat32` otherwise. FAT32 is the next best alternative and `mkfs.fat` a hard dependency due to the ESP partition anyway (see #96).

--- multios-usb.sh
+++ multios-usb.sh
@@ -10,7 +10,7 @@ set -eo pipefail
 
 # Defaults
 scriptname=$(basename "$0")
-fs_type="exfat"
+fs_type="$([ -x "$(command -v "mkfs.exfat" 2>/dev/null)" ] && echo "exfat" || echo "fat32")"
 data_size=""
 efi_size="25M"
 data_label="MultiOS-USB"
